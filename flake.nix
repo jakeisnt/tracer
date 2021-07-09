bd16ca949893e7fbd9e6c0387f7515af480d981b
Jacob Chvatal <jake@isnt.online>
2021-07-09 16:24:34 -0700

    start

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "rust-game-bevy";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-21.05";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        bintools_lld = pkgs.wrapBintoolsWith {
+          bintools = pkgs.llvmPackages_10.bintools;
+        };
+        inherit (lib) attrValues;
+        mkPkgs = pkgs: extraOverlays:
+          import pkgs {
+            inherit system;
+            config = { allowUnfree = true; };
+          };
+          pkgs = mkPkgs nixpkgs [ ];
+          lib = nixpkgs.lib;
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rustup
+            rustc
+            rust-analyzer
+            rustfmt
+            cargo
+            lld
+          ];
+        };
+      });
+}
