16a8a0aa90356ca051aa59aaaafaba1df49c76bb
Jacob Chvatal <jake@isnt.online>
2021-08-20 13:44:24 -0700

    camera zoomed out

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ mod sphere;
 mod camera;
 mod material;
 
-use vec::{Point3, Color};
+use vec::{Vec3, Point3, Color};
 use ray::Ray;
 use hit::{Hit, World};
 use sphere::Sphere;
@@ -27,13 +27,13 @@ fn ray_color(r: &Ray, world: &World, depth: u64) -> Color {
         if let Some((attenuation, scattered)) = rec.mat.scatter(r, &rec) {
             attenuation * ray_color(&scattered, world, depth - 1)
         } else {
-            Color::new(0.0, 0.0, 0.0)
-        }
+                Color::new(0.0, 0.0, 0.0)
+            }
     } else {
-        let unit_direction = r.direction().normalized();
-        let t = 0.5 * (unit_direction.y() + 1.0);
-        (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
-    }
+            let unit_direction = r.direction().normalized();
+            let t = 0.5 * (unit_direction.y() + 1.0);
+            (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+        }
 }
 
 fn main() -> () {
@@ -45,20 +45,32 @@ fn main() -> () {
     const MAX_DEPTH: u64 = 5;
 
     // World
-    let r: f64 = (std::f64::consts::PI / 4.0).cos();
     let mut world = World::new();
 
-    let mat_left = Rc::new(Lambertian::new(Color::new(0.0, 0.0, 1.0)));
-    let mat_right = Rc::new(Lambertian::new(Color::new(1.0, 0.0, 0.0)));
+    let mat_ground = Rc::new(Lambertian::new(Color::new(0.8, 0.8, 0.0)));
+    let mat_center = Rc::new(Lambertian::new(Color::new(0.1, 0.2, 0.5)));
+    let mat_left = Rc::new(Dielectric::new(1.5));
+    let mat_left_inner = Rc::new(Dielectric::new(1.5));
+    let mat_right = Rc::new(Metal::new(Color::new(0.8, 0.6, 0.2), 1.0));
 
-    let sphere_left = Sphere::new(Point3::new(-r, 0.0, -1.0), r, mat_left);
-    let sphere_right = Sphere::new(Point3::new(r, 0.0, -1.0), r, mat_right);
+    let sphere_ground = Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0, mat_ground);
+    let sphere_center = Sphere::new(Point3::new(0.0, 0.0, -1.0), 0.5, mat_center);
+    let sphere_left = Sphere::new(Point3::new(-1.0, 0.0, -1.0), 0.5, mat_left);
+    let sphere_left_inner = Sphere::new(Point3::new(-1.0, 0.0, -1.0), -0.45, mat_left_inner);
+    let sphere_right = Sphere::new(Point3::new(1.0, 0.0, -1.0), 0.5, mat_right);
 
+    world.push(Box::new(sphere_ground));
+    world.push(Box::new(sphere_center));
     world.push(Box::new(sphere_left));
+    world.push(Box::new(sphere_left_inner));
     world.push(Box::new(sphere_right));
 
     // Camera
-    let cam = Camera::new(90.0, ASPECT_RATIO);
+    let cam = Camera::new(Point3::new(-2.0, 2.0, 1.0),
+        Point3::new(0.0, 0.0, -1.0),
+        Vec3::new(0.0, 1.0, 0.0),
+        90.0,
+        ASPECT_RATIO);
 
     println!("P3");
     println!("{} {}", IMAGE_WIDTH, IMAGE_HEIGHT);
